14f773c3d990356627680abb50aeeda2a6661893
Görkem Başaran <105394833+gorkembasaran@users.noreply.github.com>
2025-07-18 17:26:57 +0300

    updated

--- ios/RNImageSizeAndroid.h
+++
@@ -1,4 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface RNImageSizeAndroid : NSObject <RCTBridgeModule>
-@end
--- ios/RNImageSizeAndroid.m
+++
@@ -1,7 +0,0 @@
-#import "RNImageSizeAndroid.h"
-
-@implementation RNImageSizeAndroid
-
-RCT_EXPORT_MODULE()
-
-@end
